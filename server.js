5cc7a299ea02dafbacd572dd587190bf102e097f
tmerlier <tmerlier@student.42.fr>
2018-05-18 14:44:07 +0200

    new /report route

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
-const {omit} = require('lodash')
 
-const wrap = require('./lib/util/wrap')
-
-const datasets = require('./datasets.json')
+const datasets = require('./db/datasets.json')
 
 const app = express()
 
 app.use(cors())
 
-app.get('/datasets', wrap(async () => {
-  return datasets.map(dataset => {
-    return omit(dataset, 'report')
-  })
-}))
+app.get('/datasets', (req, res) => {
+  res.send(datasets)
+})
 
-app.get('/datasets/:id', wrap(async req => {
+app.get('/datasets/:id', (req, res) => {
   const {id} = req.params
-  return datasets.find(dataset => dataset.id === id)
-}))
+  res.send(datasets.find(dataset => dataset.id === id))
+})
+
+app.get('/datasets/:id/report', (req, res) => {
+  const {id} = req.params
+  const dataset = datasets.find(dataset => dataset.id === id)
+  const fileName = path.join(__dirname, 'db', 'reports', `${dataset.id}.json`)
+
+  res.sendFile(fileName)
+})
 
 const port = process.env.PORT || 5000
 
