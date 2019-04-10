73dde11d3a91782ec86edf168cae6d7617729628
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-04-10 17:48:19 +0200

    Remove old express wrapper

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const cors = require('cors')
 const Keyv = require('keyv')
 const morgan = require('morgan')
-const wrap = require('./lib/util/wrap')
+const w = require('./lib/util/w')
 const mongo = require('./lib/util/mongo')
 
 const fantoirDatabase = new Keyv(`sqlite://${process.env.FANTOIR_DB_PATH}`)
@@ -17,24 +17,18 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'))
 }
 
-app.get('/fantoir/:codeCommune', wrap(async req => {
+app.get('/fantoir/:codeCommune', w(async (req, res) => {
   const {codeCommune} = req.params
   const fantoirCommune = await fantoirDatabase.get(codeCommune)
   if (!fantoirCommune) {
-    throw notFound('Commune non présente dans FANTOIR')
+    return res.status(404).send({code: 404, message: 'Commune non présente dans FANTOIR'})
   }
-  return {raw: fantoirCommune}
+  res.send({raw: fantoirCommune})
 }))
 
 app.use('/ban', require('./lib/ban2bal'))
 app.use('/datasets', require('./lib/datasets'))
 app.use('/publication', require('./lib/publication'))
-
-function notFound(message) {
-  const error = new Error(message)
-  error.notFound = true
-  return error
-}
 
 async function main() {
   await mongo.connect()
