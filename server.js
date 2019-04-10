71c207382f9ef63c34889a41c077869a64d764cc
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-04-10 17:13:53 +0200

    Move /datasets stuff to its own directory

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const morgan = require('morgan')
 const wrap = require('./lib/util/wrap')
 const mongo = require('./lib/util/mongo')
 const {createCommunesExtraction, createDepartementExtraction} = require('./lib/ban/extract')
-const {getDatasets, getReport, getSummary, getCommune, getVoie, computeStats} = require('./lib/datasets')
 
 const fantoirDatabase = new Keyv(`sqlite://${process.env.FANTOIR_DB_PATH}`)
 
@@ -43,46 +42,6 @@ app.get('/ban/extract', wrap(async (req, res) => {
   return extraction
 }))
 
-app.use('/datasets', wrap(async req => {
-  req.datasets = await getDatasets()
-}))
-
-app.param('datasetId', (req, res, next, id) => {
-  req.dataset = req.datasets.find(d => d.id === id)
-  if (!req.dataset) {
-    return res.sendStatus(404)
-  }
-  next()
-})
-
-app.get('/datasets', (req, res) => {
-  res.send(req.datasets)
-})
-
-app.get('/datasets/stats', (req, res) => {
-  res.send(computeStats(req.datasets))
-})
-
-app.get('/datasets/:datasetId', (req, res) => {
-  res.send(req.dataset)
-})
-
-app.get('/datasets/:datasetId/report', wrap(req => {
-  return getReport(req.dataset.id)
-}))
-
-app.get('/datasets/:datasetId/data/summary', wrap(req => {
-  return getSummary(req.dataset.id)
-}))
-
-app.get('/datasets/:datasetId/data/:codeCommune', wrap(req => {
-  return getCommune(req.dataset.id, req.params.codeCommune)
-}))
-
-app.get('/datasets/:datasetId/data/:codeCommune/:codeVoie', wrap(req => {
-  return getVoie(req.dataset.id, req.params.codeCommune, req.params.codeVoie)
-}))
-
 app.get('/fantoir/:codeCommune', wrap(async req => {
   const {codeCommune} = req.params
   const fantoirCommune = await fantoirDatabase.get(codeCommune)
@@ -92,6 +51,7 @@ app.get('/fantoir/:codeCommune', wrap(async req => {
   return {raw: fantoirCommune}
 }))
 
+app.use('/datasets', require('./lib/datasets'))
 app.use('/publication', require('./lib/publication'))
 
 function notFound(message) {
